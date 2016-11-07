9e577bcb53a768c9f2b5b41feddb1db9da04af7e
shang company <9212219@163.com>
2016-11-07 11:55:13 +0800

    会员卡详情，会员等级，生成会员卡样式

--- Apps/Admin/Controller/CusController.class.php
+++ Apps/Admin/Controller/CusController.class.php
@@ -111,21 +111,24 @@ class CusController extends CommonController {
             $this->assign('erate', D("Erate")->getErate());
             $this->display();
         }else{
-
-            $upload = new \Think\Upload();// 实例化上传类
-            $upload->maxSize   =     3145728 ;// 设置附件上传大小
-            $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-            $upload->rootPath  =     './Public/Uploads/'; // 设置附件上传根目录
-            $upload->savePath  =     ''; // 设置附件上传（子）目录
-            // 上传文件 
-            $info   =   $upload->upload();
-            if(!$info) {// 上传错误提示错误信息
-                $result['message']=$upload->getError();
-                $result['status']=false;  
-                $this->ajaxReturn($result);
-            }else{// 上传成功
-                $photo = $upload->rootPath.$info["photo"]["savepath"].$info["photo"]["savename"];
+            if($_FILES["photo"]["name"])
+            {
+                $upload = new \Think\Upload();// 实例化上传类
+                $upload->maxSize   =     3145728 ;// 设置附件上传大小
+                $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+                $upload->rootPath  =     './Public/Uploads/'; // 设置附件上传根目录
+                $upload->savePath  =     ''; // 设置附件上传（子）目录
+                // 上传文件 
+                $info   =   $upload->upload();
+                if(!$info) {// 上传错误提示错误信息
+                    $result['message']=$upload->getError();
+                    $result['status']=false;  
+                    $this->ajaxReturn($result);
+                }else{// 上传成功
+                    $photo = $upload->rootPath.$info["photo"]["savepath"].$info["photo"]["savename"];
+                }
             }
+            
 
             //检查会员卡号
             $cards = D("CardDetail")->getCs();
@@ -149,30 +152,34 @@ class CusController extends CommonController {
             }
 
             //检查会员性别
+            /**
             if(!I("post.gender",0,"intval"))
             {
                 $result['message']='请选择性别!';
                 $result['status']=false;  
                 $this->ajaxReturn($result);
             }
+            */
 
             //检查会员生日
+            /**
             if(!I("post.birthday"))
             {
                 $result['message']='请选择生日!';
                 $result['status']=false;  
                 $this->ajaxReturn($result);
             }
+            */
 
             $ck=A('CheckInput');
             $name=$ck->in('姓名','name','cnennumstr','',2,20);
             $levelId=$ck->in('会员等级','level_id','intval','',1,20);
-            $gender=$ck->in('性别','gender','intval','',1,20);
-            $idNo=$ck->in('身份证','id_no','cnennumstr','',16,18);
+            $gender=$ck->in('性别','gender','intval',0,0,0);
+            $idNo=$ck->in('身份证','id_no','cnennumstr','',0,0);
             $birthday=$ck->in('生日','birthday','date','',0,0);
-            $mobile=$ck->in('手机','mobile','intval','',11,11);
-            $address=$ck->in('联系地址','address','cnennumstr','',2,20);
-            $shopId=$ck->in('商铺','shop_id','intval','',0,0);
+            $mobile=$ck->in('手机','mobile','intval','',0,0);
+            $address=$ck->in('联系地址','address','cnennumstr','',0,0);
+            $shopId=$ck->in('商铺','shop_id','intval','',1,0);
             $jf=$ck->in('充值金额','jf','float(17,2)','',0,0);
             $pw=$ck->in('密码','pwd','cnennumstr','',6,20);
 
